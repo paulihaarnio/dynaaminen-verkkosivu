9c5113effdd35ed9c69d04f2c5aff3c699c43c37
paulihaarnio <pauli.haarnio@gmail.com>
2022-02-11 13:56:33 +0200

    toimii jotenki

--- tehtava2/src/App.js
+++ tehtava2/src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 import {useState, useEffect} from 'react'
 import axios from 'axios';
 
-const URL="https://covid-api.mmediagroup.fr/v1/cases"
-
+ 
 function App() {
+ /*const URL="https://covid-api.mmediagroup.fr/v1/cases?country=Sweden"*/
+ const [url, seturl] = useState()
  const [COUNTRY, setCOUNTRY] = useState() 
  const [template, settemplate] = useState()
  const [countryname, setcountryname] = useState();
@@ -15,32 +16,35 @@ function App() {
  const [update, setupdate] = useState()
 
   
- 
+ function handleChange(){
+  setCOUNTRY(document.getElementById("country").value)
+  seturl("https://covid-api.mmediagroup.fr/v1/cases?country=")
+}
  useEffect(() => {
+  
   document.getElementById("output1").innerHTML=""
   document.getElementById("output2").innerHTML=""
-  addCountry()
+  
 }, []);
 
+
 function addCountry(){
-  axios.get(URL).then((response)=>{
-    settemplate(response.data.Finland.All)
+  axios.get(url+COUNTRY).then((response)=>{
+    settemplate(response.data.All)
   }).catch(error=>{
     alert(error)
   })
-  
 }
 
 
+
   function Button(){
-    axios.get(URL)
-    .then(()=>{
       setcountryname(template.country)
       setconfirmed(template.confirmed)
       setdeaths(template.deaths)
       setpopulation(template.population)
       setupdate(template.updated)
-    })
+    setCOUNTRY("")
 }
 
   
@@ -50,9 +54,10 @@ function addCountry(){
       <h4>By Pauli Haarnio</h4>
       <p>Please use english name of the country and the first letter should be in capital. Example Germany not germany.</p>
       <label>Country</label>
-      <input id='country'></input> 
+      <input id='country' onChange={handleChange}></input> 
       <br/>
-      <button onClick={addCountry,Button}>Print information</button><br/>
+      <button onClick={addCountry}>Confirm country</button>
+      <button onClick={ Button}>Print information</button><br/>
       Country:
       <output>
         {countryname}
